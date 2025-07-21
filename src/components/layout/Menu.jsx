77362dcaefac33f04542a371a5de2a6b1278d331
kazuya-bionics <kazuya.economics@gmail.com>
2025-07-21 01:25:48 -0600

    add menu

---
+++ src/components/layout/Menu.jsx
@@ -0,0 +1,81 @@
+"use client"
+
+import gsap from "gsap"
+import { useGSAP } from "@gsap/react"
+import Link from "next/link"
+import { useRef, useEffect } from "react"
+
+export const Menu = ({ isOpen }) => {
+  const container = useRef(null)
+  const tl = useRef(null)
+
+  gsap.registerPlugin(useGSAP)
+  const { contextSafe } = useGSAP()
+
+  // Efecto hover por item
+  const onHoverLink = contextSafe((event) => {
+    const links = event.currentTarget.querySelectorAll(".text-link")
+    gsap.to(links, {
+      yPercent: -100,
+      duration: 0.5,
+      ease: "expo.out",
+      stagger: 0.05
+    })
+  })
+
+  const onUnhoverLink = contextSafe((event) => {
+    const links = event.currentTarget.querySelectorAll(".text-link")
+    gsap.to(links, {
+      yPercent: 0,
+      duration: 0.5,
+      ease: "expo.out",
+      stagger: 0.05
+    })
+  })
+
+  // Crear la animación una sola vez
+  useGSAP(() => {
+    tl.current = gsap.timeline({
+      paused: true,
+      defaults: { ease: "power2.inOut", duration: .8 }
+    })
+      .to(container.current, {
+        height: "100%",
+        pointerEvents: "auto"
+      })
+
+    // Comienza cerrado (oculto)
+    gsap.set(container.current, {
+      height: 0,
+      pointerEvents: "none"
+    })
+  }, [])
+
+  // Reproduce o revierte cuando cambia `isOpen`
+  useEffect(() => {
+    if (!tl.current) return
+    isOpen ? tl.current.play() : tl.current.reverse()
+  }, [isOpen])
+
+  return (
+    <div
+      ref={container}
+      className="w-full h-[0px] overflow-hidden flex flex-col justify-center px-8 bg-rose-500/75 backdrop-blur-lg fixed z-30 top-0 left-0"
+    >
+      <ul className="flex flex-col gap-y-8 text-6xl font-bold">
+        {["Cases", "About", "Services"].map((item, index) => (
+          <li
+            key={index}
+            onMouseEnter={onHoverLink}
+            onMouseLeave={onUnhoverLink}
+            className="py-1 overflow-hidden h-16 relative flex flex-col"
+          >
+            <Link className="text-link" href={`#${item.toLowerCase()}`}>{item}</Link>
+            <Link className="text-link" href={`#${item.toLowerCase()}`}>{item}</Link>
+            <div className="w-[5px] h-[2px] bg-black absolute left-0 top-[-50%] translate-y-[-50%]" />
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,19 +1,39 @@
-import Link from "next/link";
-import { FaAlignCenter } from "react-icons/fa6";
+"use client"
+
+import Link from "next/link"
+import { FaAlignCenter } from "react-icons/fa6"
+import { Menu } from "./Menu"
+import { useEffect, useState } from "react"
+import { usePathname } from "next/navigation"
 
 export const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const pathname = usePathname()
+
+  useEffect(() => {
+    setIsOpen(false)
+  }, [pathname])
+
+  const openMenu = () => {
+    setIsOpen(prev => !prev)
+  }
+
   return (
-    <div className='w-full fixed h-16 flex justify-between items-center top-0 px-8 z-10'>
+    <>
+      <div className='w-full fixed h-16 flex justify-between items-center top-0 px-8 z-40'>
         <Link href="#home">Vitta</Link>
         <ul className='hidden md:flex gap-x-4'>
-            <li>Casos</li>
-            <Link href="#about">Nosotros</Link>
-            <li>Servicios</li>
+          <li>Casos</li>
+          <Link href="#about">Nosotros</Link>
+          <li>Servicios</li>
         </ul>
-        <button className='md:hidden text-lg'>
-            <FaAlignCenter />
+        <button onClick={openMenu} className='md:hidden text-lg'>
+          <FaAlignCenter />
         </button>
         <Link href="#contact" className="border-2 border-black px-2 rounded-[45px]">Contact</Link>
-    </div>
+      </div>
+
+      <Menu isOpen={isOpen} />
+    </>
   )
 }
