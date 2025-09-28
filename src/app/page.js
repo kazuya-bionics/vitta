a45e3fe57794ebd7aa836339e6afee03d424aadc
kazuya-bionics <kazuya.economics@gmail.com>
2025-09-28 00:24:50 -0600

    conect loader

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,12 @@ import { MouseEffects } from "@/components/home/MouseEffects"
 import { Hero, About, Contact, Intersection_1 } from "@/components/home/"
 import { Video } from "@/components/home/Video"
 import { Services } from "@/components/home/Services"
+import { Loader } from "@react-three/drei"
 
 const page = () => {
   return (
     <main>
+      <Loader/>
       <Hero/>
       <Intersection_1/>
       <MouseEffects/>
