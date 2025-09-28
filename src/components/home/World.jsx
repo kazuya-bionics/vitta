41b5fddb2471976f11e84b4f1e93b8adee83d623
kazuya-bionics <kazuya.economics@gmail.com>
2025-09-27 21:55:16 -0600

    modify all home

--- src/components/home/World.jsx
+++ src/components/home/World.jsx
@@ -1,27 +1,30 @@
-"use client"
+"use client";
 
 import Spline from '@splinetool/react-spline';
 
 export const World = () => {
   return (
-    <div className='w-full px-8 h-screen flex justify-center items-center text-2xl flex-col relative'>
-        <div className='text-center opacity-75 md:text-4xl'>
-            <h3>
-                PROTEGER Y AMAR ES IGUAL,
-            </h3>
-            <h3>
-                DA EL PRIMER PASO Y ASEGURATE HOY
-            </h3>
-        </div> 
-        <div className='w-[100%] opacity-50 h-screen flex justify-center items-center absolute'>
-            <Spline
-                styles={{
-                    width: "100%",   // 👈 más delgado
-                    height: "100%",
-                }}
-                scene="https://prod.spline.design/m7LmZnZAPJcl8h4p/scene.splinecode"
-            />
-        </div>
+    <div className="w-full h-screen relative flex justify-center items-center">
+      {/* Canvas Spline de fondo con opacidad */}
+      <div className="absolute inset-0 opacity-50 flex justify-center items-center">
+        <Spline
+          styles={{
+            width: "100%",
+            height: "100%",
+          }}
+          scene="https://prod.spline.design/m7LmZnZAPJcl8h4p/scene.splinecode"
+        />
+      </div>
+
+      {/* Texto centrado sobre el canvas con opacidad */}
+      <div className="relative text-center opacity-60 md:opacity-60 px-4">
+        <h3 className="text-2xl md:text-4xl font-bold">
+          PROTEGER Y AMAR ES IGUAL,
+        </h3>
+        <h3 className="text-2xl md:text-4xl font-bold mt-2">
+          DA EL PRIMER PASO Y ASEGURATE HOY
+        </h3>
+      </div>
     </div>
-  )
-}
+  );
+};
