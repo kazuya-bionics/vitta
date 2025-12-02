a1ddfa4ae0a2a35f76436d0b7032a97449ca9bd5
kazuya-bionics <kazuya.economics@gmail.com>
2025-12-02 02:52:24 -0600

    add loader provider

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -14,7 +14,7 @@ export const Navbar = () => {
       {/* PageTransition siempre envuelto en AnimatePresence */}
 
       <div className="w-full fixed h-20 flex justify-between items-center top-0 px-8 z-40 font-medium">
-        <Link href="/" className="font-bold text-lg">VITTA</Link>
+        <Link href="/" className="font-bold px-3 py-1 bg-bg rounded-[45] text-lg">VITTA</Link>
 
         <ul className="hidden bg-white rounded-[45] text-md drop-shadow-lg py-2 px-4 md:flex gap-x-1">
           <Link href="/#work" className="hover:bg-black hover:text-white py-1 px-3 rounded-full transition duration-300">Work</Link>
@@ -26,11 +26,11 @@ export const Navbar = () => {
         <div className="flex gap-x-2 justify-center items-center">
           <button
             onClick={() => setIsOpen(prev => !prev)}
-            className="text-xl md:hidden p-2 bg-white rounded-full"
+            className="text-xl md:hidden p-2 bg-bg rounded-full"
           >
             <FaAlignRight />
           </button>
-          <Link className="hidden md:flex" href="/#contact">
+          <Link className="hidden px-3 bg-bg rounded-[45] md:flex" href="/#contact">
             <button className="rounded-md px-3 py-1 m-1 overflow-hidden relative group cursor-pointer  font-medium">
                 <span className="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-black top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"></span>
                 <span className="relative text-black transition duration-300 group-hover:text-white ease">Contact</span>
